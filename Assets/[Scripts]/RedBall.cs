1c0a8eefd010ffef7f34da32c9493d0bc4caa169
woonhak-kong <woonhak.kong@georgebrown.ca>
2022-02-16 19:33:48 -0500

    RedBall animation is implemented

--- Assets/[Scripts]/RedBall.cs
+++ Assets/[Scripts]/RedBall.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class RedBall : Enemy
 {
-   
+    public override void Start()
+    {
+        base.Start();
+        _animator = transform.GetChild(0).GetComponent<Animator>();
+    }
+    private void SetAnimatorPropertyDefault()
+    {
+        _animator.SetBool("RightDownJump", false);
+        _animator.SetBool("LeftDownJump", false);
+    }
+    protected override void SetPrivateProperties()
+    {
+        base.SetPrivateProperties();
+        SetAnimatorPropertyDefault();
 
+    }
 }
