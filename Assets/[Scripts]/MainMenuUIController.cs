5dc6d6dff2adea09990f39699f416b1470bc9bca
woonhak-kong <woonhak.kong@georgebrown.ca>
2022-03-04 13:48:39 -0500

    Reader board canvas is added

--- Assets/[Scripts]/MainMenuUIController.cs
+++ Assets/[Scripts]/MainMenuUIController.cs
@@ -3,9 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MainMenuUIController : MonoBehaviour
 {
+    public GameObject TitleCanvas;
+    public GameObject ReaderBoardCanvas;
+
+    enum MenuState
+    {
+        TITLE,
+        READER_BOARD
+    }
+    
+    private MenuState _state = MenuState.TITLE;
+
     // Start is called before the first frame update
+
     void Start()
     {
         
@@ -25,6 +38,20 @@ public class MainMenuUIController : MonoBehaviour
     public void OnClickExit()
     {
         Application.Quit();
+    }
+
+    public void OnClickReaderBoard()
+    {
+        _state = MenuState.READER_BOARD;
+        TitleCanvas.SetActive(false);
+        ReaderBoardCanvas.SetActive(true);
+    }
+
+    public void OnClickTitle()
+    {
+        _state = MenuState.TITLE;
+        TitleCanvas.SetActive(true);
+        ReaderBoardCanvas.SetActive(false);
     }
 
     
